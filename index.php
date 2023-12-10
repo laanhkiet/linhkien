31ebdcee81944ac413b2807dab358a543525f0c5
laanhkiet <150754311+laanhkiet@users.noreply.github.com>
2023-12-10 10:24:25 +0700

    admin/qldanhmuc

--- index.php
+++ index.php
@@ -1,79 +1,62 @@
 <?php 
+if(!isset($_SESSION["nguoidung"]))
+    header("location:../index.php");
+
 require("../../model/database.php");
-require("../../model/nguoidung.php");
-
-// Biến $isLogin cho biết người dùng đăng nhập chưa
-$isLogin = isset($_SESSION["nguoidung"]);
-
+require("../../model/danhmuc.php");
 
 // Xét xem có thao tác nào được chọn
 if(isset($_REQUEST["action"])){
     $action = $_REQUEST["action"];
 }
-elseif($isLogin == FALSE){  // chưa đăng nhập
-    $action="dangnhap";
-}
-else{   // mặc định
-    $action="macdinh";
+else{   // mặc định là xem danh sách
+    $action="xem";
 }
 
-$nd = new NGUOIDUNG();
-
+$dm = new DANHMUC();
+$idsua = 0;
 
 switch($action){
-    case "macdinh":               
+    case "xem":
+        $danhmuc = $dm->laydanhmuc();       
         include("main.php");
         break;
-    case "dangnhap":
-        include("login.php");
-        break;
-    case "xldangnhap":
-        $email = $_REQUEST["txtemail"];
-        $matkhau = $_REQUEST["txtmatkhau"];
-        if($nd->kiemtranguoidunghople($email,$matkhau)==TRUE){
-            $_SESSION["nguoidung"] = $nd->laythongtinnguoidung($email); // đặt biến session
-            include("main.php");
-        }
-        else{
-            include("login.php");
-        }
-        break;
-    case "dangxuat":
-        unset($_SESSION["nguoidung"]);  // hủy biến session
-        //include("login.php");         // hiển thị trang login
-        header("location:../../index.php");     // hoặc chuyển hướng ra bên ngoài (trang dành cho khách)
-        break;  
-    case "hoso":               
-        include("profile.php");
-        break; 
-    case "xlhoso":
-        $mand = $_POST["txtid"];
-        $email = $_POST["txtemail"];        
-        $sodt = $_POST["txtdienthoai"];
-        $hoten = $_POST["txthoten"];
-        $hinhanh = $_POST["txthinhanh"];
-
-        if($_FILES["fhinh"]["name"] != null){
-            $hinhanh = basename($_FILES["fhinh"]["name"]);
-            $duongdan = "../../images/users/" . $hinhanh;
-            move_uploaded_file($_FILES["fhinh"]["tmp_name"], $duongdan);
-        }
-        
-        $nd->capnhatnguoidung($mand,$email,$sodt,$hoten,$hinhanh);
-
-        $_SESSION["nguoidung"] = $nd->laythongtinnguoidung($email);
-        include("main.php");        
-        break;
-
-       
-    case "matkhau":               
-        include("changepass.php");
-        break; 
-    case "doimatkhau":
-         if (isset($_POST["txtemail"]) && isset($_POST["txtmatkhaumoi"]) )
-            $nd->doimatkhau($_POST["txtemail"],$_POST["txtmatkhaumoi"]);
+    case "sua": // hiển thị form
+    	$idsua = $_GET["id"];
+        $danhmuc = $dm->laydanhmuc();       
         include("main.php");
-        break; 
+        break;
+    case "capnhat": // lưu dữ liệu sửa mới vào db
+    	// gán dữ liệu từ form
+    	$dmmoi = new DANHMUC();
+    	$dmmoi->setid($_POST["id"]);
+    	$dmmoi->settendanhmuc($_POST["ten"]);
+    	// sửa
+    	$dm->suadanhmuc($dmmoi);
+    	// load danh sách
+        $danhmuc = $dm->laydanhmuc();       
+        include("main.php");
+        break;
+    case "them":
+    	// gán dữ liệu từ form
+    	$dmmoi = new DANHMUC();
+    	$dmmoi->settendanhmuc($_POST["ten"]);
+    	// thêm
+    	$dm->themdanhmuc($dmmoi);
+    	// load danh sách
+        $danhmuc = $dm->laydanhmuc();       
+        include("main.php");
+        break;
+    case "xoa":
+    	// lấy dòng muốn xóa
+    	$dmxoa = new DANHMUC();
+    	$dmxoa->setid($_GET["id"]);
+    	// xóa
+    	$dm->xoadanhmuc($dmxoa);
+    	// load danh sách
+        $danhmuc = $dm->laydanhmuc();       
+        include("main.php");
+        break;
     default:
         break;
 }
