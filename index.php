fe897c614a6bf6b6c330090a4dbbdaec590c0fb0
ntk4902 <153402068+ntk4902@users.noreply.github.com>
2023-12-10 10:28:16 +0700

    admin/qlmathang

--- index.php
+++ index.php
@@ -4,59 +4,106 @@ if(!isset($_SESSION["nguoidung"]))
 
 require("../../model/database.php");
 require("../../model/danhmuc.php");
+require("../../model/mathang.php");
 
 // Xét xem có thao tác nào được chọn
 if(isset($_REQUEST["action"])){
     $action = $_REQUEST["action"];
 }
-else{   // mặc định là xem danh sách
+else{
     $action="xem";
 }
 
 $dm = new DANHMUC();
-$idsua = 0;
+$mh = new MATHANG();
 
 switch($action){
     case "xem":
-        $danhmuc = $dm->laydanhmuc();       
+        $mathang = $mh->laymathang();
+		include("main.php");
+        break;
+	case "them":
+		$danhmuc = $dm->laydanhmuc();
+		include("addform.php");
+        break;
+	case "xulythem":	
+		// xử lý file upload
+		$hinhanh = "images/products/" . basename($_FILES["filehinhanh"]["name"]); // đường dẫn ảnh lưu trong db
+		$duongdan = "../../" . $hinhanh; // nơi lưu file upload (đường dẫn tính theo vị trí hiện hành)
+		move_uploaded_file($_FILES["filehinhanh"]["tmp_name"], $duongdan);
+		// xử lý thêm		
+        $mathanghh = new MATHANG();
+		$mathanghh->settenmathang($_POST["txttenmathang"]);
+		$mathanghh->setmota($_POST["txtmota"]);
+		$mathanghh->setgiagoc($_POST["txtgianhap"]);
+		$mathanghh->setgiaban($_POST["txtgiaban"]);
+		$mathanghh->setsoluongton($_POST["txtsoluong"]);
+		$mathanghh->setdanhmuc_id($_POST["optdanhmuc"]);
+        $mathanghh->sethinhanh($hinhanh);
+		$mh->themmathang($mathanghh);
+		$mathang = $mh->laymathang();
+		include("main.php");
+        break;
+	case "xoa":
+		if(isset($_GET["id"])){
+            $mathanghh = new MATHANG();        
+            $mathanghh->setid($_GET["id"]);
+			$mh->xoamathang($mathanghh);
+        }
+		$mathang = $mh->laymathang();
+		include("main.php");
+		break;	
+    case "chitiet":
+        if(isset($_GET["id"])){ 
+            $m = $mh->laymathangtheoid($_GET["id"]);            
+            include("detail.php");
+        }
+        else{
+            $mathang = $mh->laymathang();        
+            include("main.php");            
+        }
+        break;
+    case "sua":
+        if(isset($_GET["id"])){ 
+            $m = $mh->laymathangtheoid($_GET["id"]);
+            $danhmuc = $dm->laydanhmuc(); 
+            include("updateform.php");
+        }
+        else{
+            $mathang = $mh->laymathang();        
+            include("main.php");            
+        }
+        break;
+    case "xulysua":
+        $mathanghh = new MATHANG();
+        $mathanghh->setid($_POST["txtid"]);
+        $mathanghh->setdanhmuc_id($_POST["optdanhmuc"]);
+        $mathanghh->settenmathang($_POST["txttenhang"]);
+        $mathanghh->setmota($_POST["txtmota"]);
+        $mathanghh->setgiagoc($_POST["txtgiagoc"]);
+        $mathanghh->setgiaban($_POST["txtgiaban"]);
+        $mathanghh->setsoluongton($_POST["txtsoluongton"]);
+        $mathanghh->setluotxem($_POST["txtluotxem"]);
+        $mathanghh->setluotmua($_POST["txtluotmua"]);
+        $mathanghh->sethinhanh($_POST["txthinhcu"]);
+
+        // upload file mới (nếu có)
+        if($_FILES["filehinhanh"]["name"]!=""){
+            // xử lý file upload -- Cần bổ dung kiểm tra: dung lượng, kiểu file, ...       
+            $hinhanh = "images/" . basename($_FILES["filehinhanh"]["name"]);// đường dẫn lưu csdl
+            $mathanghh->sethinhanh($hinhanh);
+            $duongdan = "../../" . $hinhanh; // đường dẫn lưu upload file        
+            move_uploaded_file($_FILES["filehinhanh"]["tmp_name"], $duongdan);
+        }
+        
+        // sửa mặt hàng
+        $mh->suamathang($mathanghh);         
+    
+        // hiển thị ds mặt hàng
+        $mathang = $mh->laymathang();    
         include("main.php");
         break;
-    case "sua": // hiển thị form
-    	$idsua = $_GET["id"];
-        $danhmuc = $dm->laydanhmuc();       
-        include("main.php");
-        break;
-    case "capnhat": // lưu dữ liệu sửa mới vào db
-    	// gán dữ liệu từ form
-    	$dmmoi = new DANHMUC();
-    	$dmmoi->setid($_POST["id"]);
-    	$dmmoi->settendanhmuc($_POST["ten"]);
-    	// sửa
-    	$dm->suadanhmuc($dmmoi);
-    	// load danh sách
-        $danhmuc = $dm->laydanhmuc();       
-        include("main.php");
-        break;
-    case "them":
-    	// gán dữ liệu từ form
-    	$dmmoi = new DANHMUC();
-    	$dmmoi->settendanhmuc($_POST["ten"]);
-    	// thêm
-    	$dm->themdanhmuc($dmmoi);
-    	// load danh sách
-        $danhmuc = $dm->laydanhmuc();       
-        include("main.php");
-        break;
-    case "xoa":
-    	// lấy dòng muốn xóa
-    	$dmxoa = new DANHMUC();
-    	$dmxoa->setid($_GET["id"]);
-    	// xóa
-    	$dm->xoadanhmuc($dmxoa);
-    	// load danh sách
-        $danhmuc = $dm->laydanhmuc();       
-        include("main.php");
-        break;
+
     default:
         break;
 }
--- main.php
+++ main.php
@@ -1,56 +1,42 @@
 <?php include("../inc/top.php"); ?>
 
-<h4 class="text-info">Danh sách danh mục</h4> 
+<h3>Quản lý mặt hàng</h3> 
+<br>
+<a href="index.php?action=them" class="btn btn-info">
+	<i class="align-middle" data-feather="plus-circle"></i> 
+	Thêm mặt hàng
+</a>
+<br> <br> 
 <table class="table table-hover">
-	<tr><th>ID</th><th>Tên danh mục</th><th>Sửa</th><th>Xóa</th></tr>
-	<?php 
-	foreach ($danhmuc as $d) : 
-		if($d["id"] == $idsua){ // hiển thị form
+	<tr>
+		<th>Tên mặt hàng</th>
+		<th>Giá bán</th>
+		<th>Số lượng</th>
+		<th>Hình ảnh</th>		
+		<th>Sửa</th>
+		<th>Xóa</th>
+	</tr>
+	<?php
+	foreach($mathang as $m):
 	?>
-		<tr>
-		<form method="post">
-			<input type="hidden" name="action" value="capnhat">
-			<input type="hidden" name="id" value="<?php echo $d["id"]; ?>">
-			<td><?php echo $d["id"]; ?></td>
-			<td><input class="form-control" name="ten" type="text" value="<?php echo $d["tendanhmuc"]; ?>"></td>
-			<td><input class="btn btn-success" type="submit" value="Lưu"></td>
-		</form>
-			<td><a href="index.php?action=xoa&id=<?php echo $d["id"]; ?>" class="btn btn-danger">Xóa</a></td>
-		</tr>
-
-	<?php 
-		} // end if 
-		else {
-	?>
-		<tr>
-			<td><?php echo $d["id"]; ?></td>
-			<td><?php echo $d["tendanhmuc"]; ?></td>
-			<td><a href="index.php?action=sua&id=<?php echo $d["id"]; ?>" class="btn btn-warning">Sửa</a></td>
-			<td><a href="index.php?action=xoa&id=<?php echo $d["id"]; ?>" class="btn btn-danger">Xóa</a></td>
-		</tr>
-	<?php 
-		} // end else
-	endforeach; 
+	<tr>
+		<td>
+			<a href="index.php?action=chitiet&id=<?php echo $m["id"]; ?>">
+			<?php echo $m["tenmathang"]; ?>
+			</a>	
+		</td>
+		<td><?php echo $m["giaban"]; ?></td>
+		<td><?php echo $m["soluongton"]; ?></td>
+		<td>
+			<a href="index.php?action=chitiet&id=<?php echo $m["id"]; ?>">
+			<img src="../../<?php echo $m["hinhanh"]; ?>" width="80" class="img-thumbnail"></a>
+		</td>
+		<td><a class="btn btn-warning" href="index.php?action=sua&id=<?php echo $m["id"]; ?>"><i class="align-middle" data-feather="edit"></a></td>
+		<td><a class="btn btn-danger" href="index.php?action=xoa&id=<?php echo $m["id"]; ?>"><i class="align-middle" data-feather="trash-2"></a></td>
+	</tr>
+	<?php
+	endforeach;
 	?>
 </table>
-
-<h4><a class="text-decoration-none text-info" data-bs-toggle="collapse" data-bs-target="#demo">Thêm mới</a><h4>
-
-<div id="demo" class="collapse">
-	 
-	<form method="post"> 
-		<input type="hidden" name="action" value="them">
-	<div class="row">	
-		<div class="col">
-			<input type="text" class="form-control" name="ten" placeholder="Nhập tên danh mục">
-		</div>
-		<div class="col">
-			<input type="submit" class="btn btn-info" value="Lưu">
-		</div>
-		<div class="col"></div>
-	</div>
-	</form>
-</div>
-
 
 <?php include("../inc/bottom.php"); ?>
